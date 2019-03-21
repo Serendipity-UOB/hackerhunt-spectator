13e9cfcea88beea9450a5ca8875f8d32f7908780
tillywoodfield <tillyrwoodfield@gmail.com>
2019-03-21 14:06:57 +0000

    Added basic layout/containers

--- src/App.js
+++ src/App.js
@@ -19,11 +19,39 @@ class App extends Component {
 
     render () {
         return (
-            <div className='button__container'>
-                <button className='button' onClick={this.handleClick}>
-                    Click Me
-                </button>
+            // <div className='button__container'>
+                // <button className='button' onClick={this.handleClick}>
+                    // Click Me
+                // </button>
+            // </div>
+            <div id='container'>
+
+                <div id='left-container'>
+                    <div id='map-container'>
+                        <div className='title'>Map</div>
+                        <div className='content'></div>
+                    </div>
+                    <div id='logs-container'>
+                        <div className='title'>Log</div>
+                        <div className='content'></div>
+                    </div>
+                </div>
+                <div id='right-container'>
+                    <div id='leaderboard-container'>
+                        <div className='title'>Leaderboard</div>
+                        <div className='content'></div>
+                    </div>
+                </div>
+
+                {/*<div id='top-container'>*/}
+
+                {/*</div>*/}
+                {/*// <div id='bottom-container'>*/}
+
+
+                {/*</div>*/}
             </div>
+
         )
     }
 
