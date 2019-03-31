8bcc794d6f81724fe292d00d4a305bc399ebf9ae
tillywoodfield <tillyrwoodfield@gmail.com>
2019-03-31 20:49:59 +0100

    Minor changes

--- src/Map.js
+++ src/Map.js
@@ -59,19 +59,19 @@ class Map extends Component {
             0.11*width + ', ' + 0.63*height +',' +
             0*width + ', ' + 0.63*height);
 
-        let pillarRadius = 0.02*width;
-        document.getElementById('map-pillar-1').setAttribute('r', '' + pillarRadius);
-        document.getElementById('map-pillar-1').setAttribute('cx', '' + 0.8*width);
-        document.getElementById('map-pillar-1').setAttribute('cy', '' + 0.63*height);
+        // let pillarRadius = 0.02*width;
+        // document.getElementById('map-pillar-1').setAttribute('r', '' + pillarRadius);
+        // document.getElementById('map-pillar-1').setAttribute('cx', '' + 0.8*width);
+        // document.getElementById('map-pillar-1').setAttribute('cy', '' + 0.63*height);
 
-        let pillar2_cx = 0.7*width;
-        let pillar2_cy = 0.63*height;
-        document.getElementById('map-pillar-2').setAttribute('r', '' + pillarRadius);
-        document.getElementById('map-pillar-2').setAttribute('cx', '' + pillar2_cx);
-        document.getElementById('map-pillar-2').setAttribute('cy', '' + pillar2_cy);
-        document.getElementById('map-pillar-2').style.strokeDasharray = 2 * Math.PI * pillarRadius;
-        document.getElementById('map-pillar-2').style.strokeDashoffset = 0.25 * 2 * Math.PI * pillarRadius;
-        document.getElementById('map-pillar-2').setAttribute('transform', 'rotate(180 ' + pillar2_cx + ' '+ pillar2_cy + ')');
+        // let pillar2_cx = 0.7*width;
+        // let pillar2_cy = 0.63*height;
+        // document.getElementById('map-pillar-2').setAttribute('r', '' + pillarRadius);
+        // document.getElementById('map-pillar-2').setAttribute('cx', '' + pillar2_cx);
+        // document.getElementById('map-pillar-2').setAttribute('cy', '' + pillar2_cy);
+        // document.getElementById('map-pillar-2').style.strokeDasharray = 2 * Math.PI * pillarRadius;
+        // document.getElementById('map-pillar-2').style.strokeDashoffset = 0.25 * 2 * Math.PI * pillarRadius;
+        // document.getElementById('map-pillar-2').setAttribute('transform', 'rotate(180 ' + pillar2_cx + ' '+ pillar2_cy + ')');
 
         let labRadius = 0.08*width;
         let lab_cx = 0.25*width - labRadius;
@@ -229,8 +229,8 @@ class Map extends Component {
         s.polygon().attr({id: 'map-border', stroke: '#00A6EE', fill: '#001C31', points: ''});
         s.polygon().attr({id: 'map-polygon', fill: '#002c4a', stroke: '#00A6EE', points: ''});
         s.circle().attr({id: 'map-lab', fill: '#001C31', stroke: '#00A6EE', cx: '0', cy: '0', r: '0'});
-        s.circle().attr({id: 'map-pillar-1', fill: '#001C31', stroke: '#00A6EE', cx: '0', cy: '0', r: '0'});
-        s.circle().attr({id: 'map-pillar-2', fill: '#001C31', stroke: '#00A6EE', cx: '0', cy: '0', r: '0',});
+        // s.circle().attr({id: 'map-pillar-1', fill: '#001C31', stroke: '#00A6EE', cx: '0', cy: '0', r: '0'});
+        // s.circle().attr({id: 'map-pillar-2', fill: '#001C31', stroke: '#00A6EE', cx: '0', cy: '0', r: '0',});
         this.drawMap(s);
     }
 
